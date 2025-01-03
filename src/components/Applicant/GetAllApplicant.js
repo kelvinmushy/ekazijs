b360325ade3c3ab45de0db88ab8e660ad5279eda
kelvinmushy <74298025+kelvinmushy@users.noreply.github.com>
2025-01-03 12:51:33 +0300

    fix:cv

--- src/components/Applicant/GetAllApplicant.js
+++ src/components/Applicant/GetAllApplicant.js
@@ -191,6 +191,10 @@ const GetAllApplicant = () => {
     }
   };
 
+  function removeHtmlTags(text) {
+    return text.replace(/<[^>]+>/g, ''); // This regex will remove all HTML tags
+  }
+  
 
   // Handle clicking a row to view applicant profile
   const handleRowClick = (applicantId) => {
@@ -290,116 +294,134 @@ const GetAllApplicant = () => {
         <Modal.Body>
           {applicantData && (
             <div>
-              <div className={styles.cvContainer}>
-                <div className={styles.sidebar}>
-                  <div className="profile">
-                    <img
-                      src={applicantData.logo ? `http://localhost:4000${applicantData.logo}` : 'https://via.placeholder.com/100'}
-                      alt="Profile"
-                      className={styles.profileImg}
-                    />
-                    <h2 className={styles.profileName}>{applicantData.first_name} {applicantData.last_name}</h2>
-                    <p className={styles.profileTitle}>Senior Software Engineer</p>
-                    <div className={styles.profileContact}>
-                      <p>Email: {applicantData.email}</p>
-                      <p>Phone: {applicantData.phone_number}</p>
-                    </div>
-                  </div>
-                  <div className={styles.links}>
-                    {socialMediaLinks.length > 0 ? (
-                      socialMediaLinks.map((link) => (
-                        <p key={link.id}>
-                          <a href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a>
-                        </p>
-                      ))
-                    ) : (
-                      <p>No social media links available</p>
-                    )}
+            <div className={styles.cvContainer}>
+              <div className={styles.sidebar}>
+                <div className="profile">
+                  <img
+                    src={applicantData.logo ? `http://localhost:4000${applicantData.logo}` : 'https://via.placeholder.com/100'}
+                    alt="Profile"
+                    className={styles.profileImg}
+                  />
+                  <h2 className={styles.profileName}>{applicantData.first_name} {applicantData.last_name}</h2>
+                  <p className={styles.profileTitle}>Senior Software Engineer</p>
+                  <div className={styles.profileContact}>
+                    <p>Email: {applicantData.email}</p>
+                    <p>Phone: {applicantData.phone_number}</p>
                   </div>
                 </div>
-
-                <div className={styles.mainContent}>
-                  <section className={styles.section}>
-                    <h3 className={styles.sectionTitle}>About Me</h3>
-                    <p>{applicantData.about || "No about section provided."}</p>
-                  </section>
-
-                  <section className={styles.section}>
-                    <h3 className={styles.sectionTitle}>Work Experience</h3>
-                    <ul>
-                      {experiencesData.map((experience) => (
-                        <li key={experience.id}>
-                          <h4>{experience.position} | {experience.institution}</h4>
-                          <p>
-  <strong>
-    {new Date(experience.from).toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    })} - 
-    {experience.to ==="Present" 
-      ? "Present" 
-      : new Date(experience.to).toLocaleDateString('en-US', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-  </strong>
-</p>
-
-                          <p>{experience.description}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
-
-                  <section className={styles.section}>
-                    <h3 className={styles.sectionTitle}>Education</h3>
-                    <ul>
-                      {educationalQualifications.map((qualification) => (
-                        <li key={qualification.id}>
-                          <h4>{qualification.degree}</h4>
-                          <p><strong>{qualification.institution}</strong> | Graduated in  {new Date(qualification.ended).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })}</p>
-                          <p>{qualification.description}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
-
-                  <section className={styles.section}>
-                    <h3 className={styles.sectionTitle}>Skills</h3>
-                    <div className={styles.skills}>
-                      {skills.map((skill) => (
-                        <span key={skill.id} className={styles.skill}>{skill.skill_name}</span>
-                      ))}
-                    </div>
-                  </section>
-
-                  <section className={styles.section}>
-                    <h3 className={styles.sectionTitle}>Certifications</h3>
-                    <ul>
-                      {professionalQualifications.map((qualification) => (
-                        <li key={qualification.id}>
-                          <h4>{qualification.course}</h4>
-                          <p><strong>{qualification.institution}</strong> |  {new Date(qualification.ended).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
+                <div className={styles.links}>
+                  {socialMediaLinks.length > 0 ? (
+                    socialMediaLinks.map((link) => (
+                      <p key={link.id}>
+                        <a href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a>
+                      </p>
+                    ))
+                  ) : (
+                    <p>No social media links available</p>
+                  )}
                 </div>
               </div>
+          
+              <div className={styles.mainContent}>
+                <section className={styles.section}>
+                  <h3 className={styles.sectionTitle}>About Me</h3>
+                  <p>{applicantData.about || "No about section provided."}</p>
+                </section>
+          
+                <section className={styles.section}>
+                  <h3 className={styles.sectionTitle}>Work Experience</h3>
+                  <ul>
+                    {experiencesData.map((experience) => (
+                      <li key={experience.id}>
+                        <h4>{experience.position} | {experience.institution}</h4>
+                        <p>
+                          <strong>
+                            {new Date(experience.from).toLocaleDateString('en-US', {
+                              year: 'numeric',
+                              month: 'long',
+                              day: 'numeric',
+                            })} - 
+                            {experience.to === "Present" 
+                              ? "Present" 
+                              : new Date(experience.to).toLocaleDateString('en-US', {
+                                  year: 'numeric',
+                                  month: 'long',
+                                  day: 'numeric',
+                                })}
+                          </strong>
+                        </p>
+          
+                        {/* Responsibilities Section */}
+                        <section className={styles.section}>
+                          <h3 className={styles.sectionTitle}>Responsibilities</h3>
+                          <div className={styles.responsibilitiesWrapper}>
+  {experience.responsibilities.split('\n').map((line, index) => (
+    <span key={index} className={styles.responsibilityText}>
+      {removeHtmlTags(line)} {/* Strip HTML tags from each line */}
+      <br />
+    </span>
+  ))}
+</div>
 
-              {/* Button to generate PDF */}
-            
+                        </section>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+          
+                <section className={styles.section}>
+                  <h3 className={styles.sectionTitle}>Education</h3>
+                  <ul>
+                    {educationalQualifications.map((qualification) => (
+                      <li key={qualification.id}>
+                        <h4>{qualification.programme}</h4>
+                        <p>
+                          <strong>{qualification.institution}</strong> | Graduated in  
+                          {new Date(qualification.ended).toLocaleDateString('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          })}
+                        </p>
+                        <p>{qualification.description}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+          
+                <section className={styles.section}>
+                  <h3 className={styles.sectionTitle}>Skills</h3>
+                  <div className={styles.skills}>
+                    {skills.map((skill) => (
+                      <span key={skill.id} className={styles.skill}>{skill.skill_name}</span>
+                    ))}
+                  </div>
+                </section>
+          
+                <section className={styles.section}>
+                  <h3 className={styles.sectionTitle}>Certifications</h3>
+                  <ul>
+                    {professionalQualifications.map((qualification) => (
+                      <li key={qualification.id}>
+                        <h4>{qualification.course}</h4>
+                        <p>
+                          <strong>{qualification.institution}</strong> |  
+                          {new Date(qualification.ended).toLocaleDateString('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          })}
+                        </p>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              </div>
             </div>
+            
+            {/* Button to generate PDF */}
+          </div>
+          
           )}
         </Modal.Body>
       </Modal>
